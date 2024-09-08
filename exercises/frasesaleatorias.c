f4dae4d29177c66d111ab433f4ad0e7f3f1ab687
izlyf <izadora.figueiredo@alunos.ufersa.edu.br>
2024-09-08 11:30:59 -0300

    sequential_structure

---
+++ exercises/frasesaleatorias.c
@@ -0,0 +1,13 @@
+//Questão 02
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+	printf("Olá \n");
+	printf("\nOlá");
+	printf("\n");
+	printf("\n \t Olá\n");
+	printf("Olá \n \t Olá \n");
+return(0);
+}
---
+++ exercises/helloword.c
@@ -0,0 +1,12 @@
+//Questão 01
+#include<stdio.h>
+
+int main(){
+	printf("Hello, World \n");
+	printf("\n");
+	printf("Hello, \nWorld! \n");
+	printf("\n \t Hello, World!");
+	printf("\nHello, \n \tWorld!");
+	printf("\n");
+return(0);
+}
