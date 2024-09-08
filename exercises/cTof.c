2bfc38a882e9176467d83d5026000ac910d993c2
iz <izadora.figueiredo@alunos.ufersa.edu.br>
2024-09-08 11:39:22 -0300

    Update cTof.c

---
+++ exercises/cTof.c
@@ -0,0 +1,15 @@
+//Questão 04
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	float c, f;
+	printf("Informe a temperatura em Celsius: ");
+	scanf("%f", &c);
+	
+	f = (c*1.8)+32;
+	
+	printf("A temperatura em Fahrenheit será: %.2f", f);
+return(0);
+}
